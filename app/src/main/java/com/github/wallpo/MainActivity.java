79125457f81aa3fd1582a4e5347c2e63c92b5486
DVLaD <62669119+EinSlen@users.noreply.github.com>
2021-04-18 15:09:30 +0200

    Add files via upload

---
+++ app/src/main/java/com/github/wallpo/MainActivity.java
@@ -0,0 +1,56 @@
+package com.github.wallpo;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.github.wallpoo.Wallpo;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Context mContext = MainActivity.this;
+    ImageView imageView;
+    Button wallpaper, lockscreen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        imageView = findViewById(R.id.imageview);
+        wallpaper = findViewById(R.id.setwallpaper);
+        lockscreen = findViewById(R.id.setlockscreen);
+
+
+        wallpaper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Wallpo.setMainScreenWallpaper(mContext, imageView, "Wallpaper Set");
+
+            }
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            lockscreen.setVisibility(View.VISIBLE);
+
+        } else {
+            lockscreen.setVisibility(View.GONE);
+        }
+
+        lockscreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Wallpo.setLockScreenWallpaper(MainActivity.this, imageView, "LockWallpaper Set");
+
+            }
+        });
+    }
+}
---
+++ wallpo/src/main/java/com/github/wallpoo/Wallpo.java
@@ -0,0 +1,90 @@
+package com.github.wallpoo;
+/*
+Copyright 2020 Rizwan Sayyed(sayyed rizwan)
+ */
+import android.app.Activity;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.NetworkOnMainThreadException;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Wallpo {
+
+    public static WallpaperManager setMainScreenWallpaper(Context context, ImageView imageView, String message) {
+
+        Bitmap bitmap;
+
+        WallpaperManager manager = WallpaperManager.getInstance(context);
+        try {
+            bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                manager.setBitmap(bitmap, null, true, WallpaperManager.FLAG_SYSTEM);
+            }else {
+
+                manager.setBitmap(bitmap);
+            }
+
+            if(message.isEmpty()){
+                message = "Wallpaper set successfully";
+            }
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+
+    public static WallpaperManager setLockScreenWallpaper(Activity activity, ImageView imageView, String message) {
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            WallpaperManager manager = WallpaperManager.getInstance(activity);
+
+            try {
+                manager.setBitmap(((BitmapDrawable) imageView.getDrawable()).getBitmap(), null, true, WallpaperManager.FLAG_LOCK);
+
+                if (message.isEmpty()){
+                    message = "Lock Screen Wallpaper set successfully";
+                }
+
+                Toast.makeText(activity, message, Toast.LENGTH_SHORT).show();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            Toast.makeText(activity, "Not supported with the version", Toast.LENGTH_SHORT).show();
+        }
+        return null;
+
+    }
+
+
+}
